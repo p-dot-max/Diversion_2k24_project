90d3e3adab8e4232d6a549f916c27286411d2fdd
memcpy_crpt_0(verflow_ <atulya1096@gmail.com>
2024-01-25 01:23:35 +0530

    formated the code

--- src/components/NavBar/Dropdown/Dropdown.tsx
+++ src/components/NavBar/Dropdown/Dropdown.tsx
@@ -1,34 +1,35 @@
 import TabButton from "../TabButton/TabButton";
 import { useState } from "react";
 
-export default function Dropdown( {children} : any ) {
+export default function Dropdown({ children }: any) {
+  // State to change selected button status and control dropdown
+  const [selectedTopic, setSelectedTopic] = useState<string>("");
+  const [isDropdownOpen, setIsDropdownOpen] = useState<boolean>(false);
 
-    // State to change selected button status and control dropdown 
-    const [selectedTopic, setSelectedTopic] = useState<string>('');
-    const [isDropdownOpen, setIsDropdownOpen] = useState<boolean>(false);
+  const handleTabClick = (selectedButton: string) => {
+    setSelectedTopic(selectedButton);
+    if (selectedButton === "prediction") {
+      setIsDropdownOpen(!isDropdownOpen);
+    } else {
+      setIsDropdownOpen(false);
+    }
+  };
 
-    const handleTabClick = (selectedButton: string) => {
-      setSelectedTopic(selectedButton);
-      if (selectedButton === 'prediction') {
-        setIsDropdownOpen(!isDropdownOpen);
-      } else {
-        setIsDropdownOpen(false);
-      }
-    };
-
-    return (
-        <>
-            <TabButton isSelected={selectedTopic === 'prediction'} onClick={() => handleTabClick('prediction')}>
-                {children}
-            </TabButton>
-                {isDropdownOpen && (
-                  <ul className="absolute left-0 mt-2 w-40 bg-white border rounded shadow-lg">
-                    {/* Dropdown menu */}
-                    <li className="border-b px-4 py-2 hover:bg-gray-100">Item 1</li>
-                    <li className="border-b px-4 py-2 hover:bg-gray-100">Item 2</li>
-                  </ul>
-                )}
-        </>
-    );
+  return (
+    <>
+      <TabButton
+        isSelected={selectedTopic === "prediction"}
+        onClick={() => handleTabClick("prediction")}
+      >
+        {children}
+      </TabButton>
+      {isDropdownOpen && (
+        <ul className="absolute left-0 mt-2 w-40 bg-white border rounded shadow-lg">
+          {/* Dropdown menu */}
+          <li className="border-b px-4 py-2 hover:bg-gray-100">Item 1</li>
+          <li className="border-b px-4 py-2 hover:bg-gray-100">Item 2</li>
+        </ul>
+      )}
+    </>
+  );
 }
-
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,31 +1,27 @@
 import { Link } from "react-router-dom";
 
 const NavBar = () => {
-  
   return (
     <nav className="w-3/5">
       <ul className="flex justify-between items-center mx-10 font-semibold text-black">
         <li>
-          <Link to={'/'}>Home</Link>
+          <Link to={"/"}>Home</Link>
         </li>
         <li className="relative">
-          <Link to={'/prediction'}>
-            Prediction
-          </Link>
+          <Link to={"/prediction"}>Prediction</Link>
         </li>
         <li>
-          <Link to={'/transportaion'}>Transportation</Link>
+          <Link to={"/transportaion"}>Transportation</Link>
         </li>
         <li>
-          <Link to={'/community'}>Community</Link>
+          <Link to={"/community"}>Community</Link>
         </li>
         <li>
-          <Link to={'/contacts'}>Contact</Link>
+          <Link to={"/contacts"}>Contact</Link>
         </li>
       </ul>
     </nav>
-  )
-}
+  );
+};
 
 export default NavBar;
-
