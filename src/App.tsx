234cc01760285bbadb31414bcd92d3d4a4e5a145
Kaialan <satyajit1501@gmail.com>
2024-01-24 22:00:01 +0530

    Setup Routing routes has been properly set up, change root css to 0 margin,padding,min-width , Next step to style up Navbar,footer and then procced with Home section 👍

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
+import NavBar from "./components";
+import { Outlet } from "react-router-dom";
+import Footer from "./components/Footer/Footer";
 
 function App() {
-
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <main className="static">
+
+        {/* Navbar parent section */}
+        <nav className="flex justify-center items-center w-full h-20">
+          <NavBar />
+        </nav>
+
+        {/* Outlet renders children paths i.e, in our case the paths in navbar */}
+        <Outlet />
+
+        {/* Footer parent section */}
+        <footer className="flex justify-center items-center w-full h-2/5 absolute bottom-0 bg-slate-500">
+          <Footer />
+        </footer>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,8 +1,25 @@
+import { Link } from "react-router-dom";
 const NavBar = () => {
   return (
-    <div>
-        NavBar
-    </div>
+    <nav className="w-3/5">
+      <ul className="flex justify-between items-center mx-10 font-semibold text-black">
+        <li>
+          <Link to={'/'}>Home</Link>
+        </li>
+        <li>
+          <Link to={'/prediction'}>Prediction</Link>
+        </li>
+        <li>
+          <Link to={'/transportaion'}>Transportation</Link>
+        </li>
+        <li>
+          <Link to={'/community'}>Community</Link>
+        </li>
+        <li>
+          <Link to={'/contacts'}>Contact</Link>
+        </li>
+      </ul>
+    </nav>
   )
 }
 
